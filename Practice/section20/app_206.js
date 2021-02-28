20f7469b0c7dafae74fedb7a80c5aab4ce6671f6
ionut-mntn <43119550+ionut-mntn@users.noreply.github.com>
2021-02-28 22:29:29 +0200

    Revazut tables & forms

--- Practice/section20/app_206.js
+++ Practice/section20/app_206.js
@@ -2,4 +2,4 @@ function greet(firstName = 'cox') {
     console.log(`firstname is: ${firstName} `);
 }
 
-// !!daca dam doar greet() si argumentul este expected, atunci valoarea sa ( a argumentului ) va fi undefined!! 
+// !!daca dam doar greet() si argumentul este expected, atunci valoarea sa ( a argumentului ) va fi undefined!!
